485cc2da91de206596dd02a9881c45462c6ebd8d
markyyy <egariando619@gmail.com>
2023-07-24 01:46:39 +0800

    ok

--- includes/header.php
+++ includes/header.php
@@ -2,5 +2,11 @@
               <div class="sidebar-toggle-box">
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
-           <div class="navbar-brand sm-8"><b>e:Reklamo</b></div> 
+              <div class="col">
+                <!-- Navbar Brand -->
+                <div class="navbar-brand">
+                  <!--  <img src="../Faculty/img/CMS.png" alt="e:Reklamo" class="img-circle" width="45" height="45" > -->
+                    <b>e:Reklamo</b>
+                </div>
+            </div>
         </header>
